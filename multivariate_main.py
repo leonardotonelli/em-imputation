167d57037346b36f51b24e1006479f417a9d2ab4
Leonardo Tonelli <leonardotonelli03@gmail.com>
2025-11-29 20:26:31 +0100

    update

---
+++ multivariate_main.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from data_generation import *
+from EM import *
+
+
+means_to_test = [
+    [],
+    [],
+    [],
+]
+
+cov_to_test = [
+    [[]],
+    [[]],
+    [[]]
+]
+
+n_samples_to_test = [
+    1,
+    10,
+    ...
+]
+
+percentages_to_test = [
+    0.1, 
+    0.2, 
+    0.3
+    ]
+
+path = ""
+
